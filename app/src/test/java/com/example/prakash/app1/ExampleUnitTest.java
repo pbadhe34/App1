9f8b5b2201d42991db3cf44ce27008e26e833c65
Prakash Badhe <prakash.badhe@agilesoft.in>
2017-09-19 15:57:15 +0530

    Updating from android sdk here

--- app/src/test/java/com/example/prakash/app1/ExampleUnitTest.java
+++ app/src/test/java/com/example/prakash/app1/ExampleUnitTest.java
@@ -17,6 +17,6 @@ public class ExampleUnitTest {
 
     public void test1()
     {
-        System.out.print(" Text is updated here");
+        System.out.print(" Text is updated here nn");
     }
 }
